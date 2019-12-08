09508b13c18e807e9d57de201fd6880d1074c5fa
Emmett <eweisz@yuhsbstudents.org>
2019-12-07 21:12:46 -0500

    Added Subclasses for red alliance
    Both red and blue classes must be tested to make sure this works. If anything goes wrong, we should just copy-paste the entire code and update everything twice.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedLoadingZoneAuto5361.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="Red Loading Zone Auto", group="Linear Opmode")
+public class RedLoadingZoneAuto5361 extends LoadingZoneAuto5361 {
+    @Override
+    public boolean getIsBlueAlliance() {return false;}
+}
