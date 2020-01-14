27078951a715631b02ea6675ec8cf714e0208760
Peretz L <prplevin11@gmail.com>
2020-01-14 15:48:06 -0500

    Revert "dw abt it"
    This reverts commit 77a3fa3b5acc6dba4adb44b8bd5391bce83911fe.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp5361.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp5361.java
@@ -27,7 +27,7 @@ public class TeleOp5361 extends LinearOpMode {
 
     // Declare OpMode members. //RECODING FOR 6 MOTORS, 4 SERVOS
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor motorFL, motorFR, strafeMotor, clawTower;
+    private DcMotor motorFL, motorFR, motorBL, motorBR, strafeMotor, clawTower;
     private Servo sClawR, sClawL, fGripR, fGripL; //fGrip : foundationGripRight/Left, sClaw : stoneClawRight/Left
     private ColorSensor leftColor, rightColor;
     private String driveMode = "Tank Control"; //Values are "Tank Control" and "Joystick Control".
@@ -57,8 +57,8 @@ public class TeleOp5361 extends LinearOpMode {
         telemetry.addData("Status", "Initialized - Setting Up");
         telemetry.update();
 
-        //motorBL = hardwareMap.dcMotor.get("motorBL");
-        //motorBR = hardwareMap.dcMotor.get("motorBR");
+        motorBL = hardwareMap.dcMotor.get("motorBL");
+        motorBR = hardwareMap.dcMotor.get("motorBR");
         motorFL = hardwareMap.dcMotor.get("motorFL");
         motorFR = hardwareMap.dcMotor.get("motorFR");
         strafeMotor = hardwareMap.dcMotor.get("motorM");
@@ -68,12 +68,12 @@ public class TeleOp5361 extends LinearOpMode {
         sClawR = hardwareMap.servo.get("blockClawR");
         fGripL = hardwareMap.servo.get("foundationGripL");
         fGripR = hardwareMap.servo.get("foundationGripR");
-        leftColor = hardwareMap.colorSensor.get("Lcolor");
-        rightColor = hardwareMap.colorSensor.get("Rcolor");
+        leftColor = hardwareMap.colorSensor.get("colorL");
+        rightColor = hardwareMap.colorSensor.get("colorR");
 
         //switch these if they are going backward
-        //motorBL.setDirection(DcMotor.Direction.REVERSE);
-        //motorBR.setDirection(DcMotor.Direction.FORWARD);
+        motorBL.setDirection(DcMotor.Direction.FORWARD);
+        motorBR.setDirection(DcMotor.Direction.REVERSE);
         motorFL.setDirection(DcMotor.Direction.REVERSE);
         motorFR.setDirection(DcMotor.Direction.FORWARD);
         strafeMotor.setDirection(DcMotor.Direction.FORWARD);
@@ -91,14 +91,14 @@ public class TeleOp5361 extends LinearOpMode {
 
         motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motorFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         strafeMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        //motorBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        //motorBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         strafeMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         telemetry.addData("Status", "Initialized and Set Up");
@@ -108,6 +108,8 @@ public class TeleOp5361 extends LinearOpMode {
     {
         double leftPower;
         double rightPower;
+        double leftPrecision;
+        double rightPrecision;
 
      /* if (!yWasPressed & gamepad1.y) {
             if (driveMode == "Tank Control") {driveMode = "Joystick Control";}
@@ -120,26 +122,42 @@ public class TeleOp5361 extends LinearOpMode {
         if (driveMode == "Tank Control") {
             leftPower = -gamepad1.left_stick_y;
             rightPower = -gamepad1.right_stick_y;
+            leftPrecision = -gamepad2.left_stick_y;
+            rightPrecision = -gamepad2.left_stick_y;
 
 
         }/* else if (driveMode == "Joystick Control") {
             leftPower = Range.clip(-gamepad1.right_stick_y + gamepad1.right_stick_x, -1, 1);
             rightPower = Range.clip(-gamepad1.right_stick_y - gamepad2.right_stick_x, -1, 1);
-        } */ else {telemetry.addData("Error", "Unacceptable driveMode"); telemetry.update(); leftPower = 0; rightPower = 0;}
+        } */ else {telemetry.addData("Error", "Unacceptable driveMode"); telemetry.update(); leftPower = 0; rightPower = 0; leftPrecision = 0; rightPrecision = 0;}
 
         // write the values to the motors
-        //motorBL.setPower(leftPower);
-        //motorBR.setPower(rightPower);
+        //driver A can give drive to driver B and vice versa for PowerDrive and PrecisionDrive
+        if(gamepad1.left_bumper && gamepad1.right_bumper) {
+            motorBL.setPower(leftPrecision);
+            motorBR.setPower(rightPrecision);
+            motorFL.setPower(leftPrecision);
+            motorFR.setPower(rightPrecision);
+        }
+        if(gamepad2.left_bumper && gamepad2.right_bumper) {
+            motorBL.setPower(leftPower);
+            motorBR.setPower(rightPower);
             motorFL.setPower(leftPower);
             motorFR.setPower(rightPower);
+        }
 
+        //foundation Grab //works
+        if (gamepad2.dpad_up)   {fGripL.setPosition(.13); fGripR.setPosition(.1);} //up
+        if (gamepad2.dpad_down) {fGripL.setPosition(.78); fGripR.setPosition(.75);} //down
 
-        if (gamepad1.right_bumper) {fGripL.setPosition(.13); fGripR.setPosition(.1);} //up
-        if (gamepad1.left_bumper) {fGripL.setPosition(.78); fGripR.setPosition(.75);} //down
-        if (gamepad1.b) {sClawL.setPosition(.05); sClawR.setPosition(.12);} //open - originally both .1 //might have to make these open more for robotV2
-        if (gamepad1.x) {sClawL.setPosition(.34); sClawR.setPosition(.47);} // close - originally both .4
-        if (gamepad1.y) {clawTower.setPower(0.7);} //tower up
-        if (gamepad1.a) {clawTower.setPower(-.7);} //tower down
+        //tower lift //works
+        if (gamepad2.y)         {clawTower.setPower(1); sleep(300); clawTower.setPower(0);} //tower up
+        if (gamepad2.a)         {clawTower.setPower(-1); sleep(300); clawTower.setPower(0);} //tower down
+
+        //stone grab //works
+        if (gamepad1.x)         {sClawL.setPosition(.05); sClawR.setPosition(.12);} //open - originally both .1 //might have to make these open more for robotV2
+        if (gamepad1.b)         {sClawL.setPosition(.34); sClawR.setPosition(.47);} // close - originally both .4
+
 
 
         //strafeMotor.setPower(gamepad1.left_trigger - gamepad1.right_trigger); //[thousand]
@@ -149,9 +167,9 @@ public class TeleOp5361 extends LinearOpMode {
             //telemetry.addData("Claw Position", gamepad1.right_trigger/10);
         }telemetry.addData("Claw Position", bClawM.getPosition()); */
 
-        String teleFormat = "leftPower (%.2f), rightPower (%.2f)";
+        String teleFormat = "leftPower (%.2f), rightPower (%.2f), leftPrecision (%.2f), rightPrecision (%.2f)";
         telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", teleFormat, leftPower, rightPower);
+        telemetry.addData("Motors", teleFormat, leftPower, rightPower, leftPrecision, rightPrecision);
         telemetry.update();
     }
 }
