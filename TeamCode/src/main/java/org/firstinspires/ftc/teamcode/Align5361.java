9f742ae0af00b54f5122e75b8f0713f27b92d1ea
Emmett <eweisz@yuhsbstudents.org>
2020-02-18 19:41:51 -0500

    Fixed telemetry in color auto and got rid of slow start. We can't use the strafe wheel to move long distances because of the diagonal drift, so we will need to rotate to bring the stone across. Added a class to slowly move the robot backwards so that it's aligned with the skystone.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Align5361.java
@@ -0,0 +1,135 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+/**
+ * Instructions:
+ * Align the robot in front of the center stone without squishing the squishy.
+ * Run this code to move the robot directly backward so it will line up.
+ */
+@Autonomous(name="Align with stone", group="Linear Opmode")
+public class Align5361 extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor motorBL, motorBR, motorFL, motorFR;
+    private static final double COUNTS_PER_MOTOR_REV = 1440;    // eg: TETRIX Motor Encoder
+    private static final double DRIVE_GEAR_REDUCTION = 1.0;     // This is < 1.0 if geared UP
+    private static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
+    private static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+
+    @Override
+    public void runOpMode() {
+        setUp();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+        encoderDrive(0.3, -23, -23, 4);
+    }
+
+    private void setUp(){ //account for alliance
+        telemetry.addData("Status", "Initialized - Setting Up");
+        telemetry.update();
+
+        // Exchange right and left if in the red alliance.
+        // We may need to assign a different variable to represent motors that don't switch for vuforia,
+        // if the camera is on the side of the phone.
+
+        motorBL = hardwareMap.dcMotor.get("motorBL");
+        motorFL = hardwareMap.dcMotor.get("motorFL");
+        motorBR = hardwareMap.dcMotor.get("motorBR");
+        motorFR = hardwareMap.dcMotor.get("motorFR");
+
+        //resetting encoders & waiting
+        telemetry.addData("Status", "Resetting Encoder");
+        telemetry.update();
+
+        motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+    }
+
+    protected void encoderDrive(double speed,
+                                double leftInches, double rightInches,
+                                double timeoutS) { // middle inputs are how many inches to travel
+        int newFRTarget;
+        int newFLTarget;
+        int newBLTarget;
+        int newBRTarget;
+        int newSMTarget;
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newFRTarget = motorFR.getCurrentPosition()     + (int) (rightInches   * COUNTS_PER_INCH);
+            newFLTarget = motorFL.getCurrentPosition()     + (int) (leftInches  * COUNTS_PER_INCH);
+            newBLTarget = motorBL.getCurrentPosition()     + (int) (leftInches   * COUNTS_PER_INCH);
+            newBRTarget = motorBR.getCurrentPosition()     + (int) (rightInches  * COUNTS_PER_INCH);
+
+            motorFR.setTargetPosition(newFRTarget);
+            motorFL.setTargetPosition(newFLTarget);
+            motorBL.setTargetPosition(newBLTarget);
+            motorBR.setTargetPosition(newBRTarget);
+
+            // Turn On RUN_TO_POSITION
+            motorFR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorFL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorBL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorBR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            motorFR.setPower(Math.abs(speed));
+            motorFL.setPower(Math.abs(speed));
+            motorBL.setPower(Math.abs(speed));
+            motorBR.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (motorFR.isBusy() && motorFL.isBusy() && motorBL.isBusy() && motorBR.isBusy())) {
+
+                // Display it for the driver.
+                /* telemetry.addData("Path1", "Running to %7d :%7d :%7d :%7d :%7d",
+                        newFRTarget, newFLTarget, newBLTarget, newFRTarget, newSMTarget);
+                telemetry.addData("Path2", "Running at %7d :%7d :%7d :%7d :%7d",
+                        motorFR.getCurrentPosition(),
+                        motorFL.getCurrentPosition(),
+                        motorBL.getCurrentPosition(),
+                        motorBR.getCurrentPosition(),
+						strafeMotor.getCurrentPosition()); */
+            }
+
+            telemetry.addData("Encoder Drive", "Finished in %.2f s/%f", runtime.seconds(), timeoutS);
+            telemetry.update();
+
+            // Stop all motion;
+            motorFR.setPower(0);
+            motorFL.setPower(0);
+            motorBL.setPower(0);
+            motorBR.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            motorFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            motorBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            motorBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+    }
+}
